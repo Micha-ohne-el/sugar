dd1f8023577cbf4412aed9450ff24dc1d54a038b
Micha Lehmann <michalehmann0112@gmail.com>
2021-01-16 15:00:40 +0100

    Move grammars to YAML
    Saves a lot of space is much easier to work with imo
    JSON support is still included but will be removed eventually

--- my-language/interpreter/load_grammars.py
+++ my-language/interpreter/load_grammars.py
@@ -1,4 +1,5 @@
 import json
+import yaml
 from typing import Iterable
 from pathlib import Path
 
@@ -13,5 +14,13 @@ def load_grammars_json(files: Iterable = None) -> dict:
 
     return syntaxes
 
+def load_grammars_yaml(files: Iterable = None) -> dict:
+    if not files:
+        p = ROOT / "grammars"
+        files = (open(f) for f in p.rglob("*.yaml"))
 
-load_grammars = load_grammars_json
+    syntaxes = {Path(f.name).stem: yaml.full_load(f) for f in files}
+
+    return syntaxes
+
+load_grammars = load_grammars_yaml
